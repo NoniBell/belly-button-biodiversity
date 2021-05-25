09cb8fbf4da79913ac0f3799f85294054580e94b
Noni Bell <nonibell2020@u.northwestern.edu>
2021-05-25 13:21:07 -0500

    added base files

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Activity 6 has sorted, sliced horizontal bar plots 
+// and there is a horizontal bar plot in the homework
+// The homework also has a standard scatter plot
+// Activity 8 will have some helpful info about how to use the dropdown for your homework.
+// Also, Day 1 Activity 3 shows how to append HTML and data to the HTML.  
+// You will need to do something similar to populate your dropdown in the homework 
+// and to make the small meta-data table/list in the homework
