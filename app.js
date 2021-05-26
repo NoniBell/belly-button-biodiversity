7afbba19d201249cef3661f4e644767fa3dde576
Noni Bell <nonibell2020@u.northwestern.edu>
2021-05-25 21:28:09 -0500

    rearranged directory

--- app.js
+++ app.js
@@ -5,3 +5,16 @@
 // Also, Day 1 Activity 3 shows how to append HTML and data to the HTML.  
 // You will need to do something similar to populate your dropdown in the homework 
 // and to make the small meta-data table/list in the homework
+                //HOW TO DEPLOY TO GITHUB
+//go to settings tab on repo page
+//go to "pages" on left side
+//make sure branch is main
+//save
+//it'll take a couple minutes to launch the page
+
+
+//read and log the samples.json
+const samples = d3.json("samples.json");
+console.log(samples);
+
+//create horizontal bar chart
